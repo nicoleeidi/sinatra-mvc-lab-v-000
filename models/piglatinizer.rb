1cf8f9c0fbb1442434f1fd2da3307d8927188654
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-15 17:38:55 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,20 +3,47 @@ class PigLatinizer
   def initialize(text)
     @text=text
   end
+  def vowel(l)
+    if l == "a"||l =="e"||l == "O"||l =="i"||l =="o"||l =="u"||l =="A"||l =="I"||l =="E"||l =="U"
+      return true
+    else
+      return false
   def convert
     #if text.include?(" ")
       array= @text.split(" ")
     newarray= array.map do |word|
 #binding.pry
-      if word[0] == "a"||word[0] =="e"||word[0] == "O"||word[0] =="i"||word[0] =="o"||word[0] =="u"||word[0] =="A"||word[0] =="I"||word[0] =="E"||word[0] =="U"
+      #if word[0] == "a"||word[0] =="e"||word[0] == "O"||word[0] =="i"||word[0] =="o"||word[0] =="u"||word[0] =="A"||word[0] =="I"||word[0] =="E"||word[0] =="U"
+      if vowel(word[0]) == true
         word += "way"
 
-      else
-        binding.pry
+      elsif vowel(word[1]) == true
+      first= word.slice(0)
+      word.slice!(0)
+      word += first
+      word += "ay"
+    elsif vowel(word[2]) == true
         first= word.slice(0)
-        word.slice!(0)
-        word += first
-        word += "ay"
+         second= word.slice(1)
+         word.slice!(0,2)
+         word += first + second + "ay"
+      elsif vowel(word[3]) == true
+        first= word.slice(0)
+         second= word.slice(1)
+         third=word.slice(2)
+         word.slice!(0,3)
+         word += first + second + third + "ay"
+      else
+        first= word.slice(0)
+         second= word.slice(1)
+         third=word.slice(2)
+         fourth=word.slice(3)
+         word.slice!(0,4)
+         word += first + second + third + fourth + "ay"
+
+
+      # word.chars.map do |letter|# or word.each_char
+        #           if letter== "a"||letter=="e"||letter=="i"||letter="o"||letter=="u"
 
       end
     end
