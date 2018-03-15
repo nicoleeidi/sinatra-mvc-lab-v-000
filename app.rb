3cb782530bee65a8bdc7abf13c2e755251fd8e71
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-15 03:29:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
-  post '/' do
-
-  end
+  post '/piglatinize' do
+    text_from_user= params[:user_phrase]
+    @text_from_user= PigLatinizer.new(text_from_user)
+    erb :newview
   end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,30 @@
-class PigLatinize
+class PigLatinizer
   attr_accessor :text
   def initialize(text)
-    @text=text.downcase
+    @text=text
   end
   def convert
+    #if text.include?(" ")
+      array= @text.split(" ")
+    newarray= array.map do |word|
+#binding.pry
+      if word[0] != "a"||"e"||"i"||"o"||"u"||"A"||"I"||"E"||"U"||"O"
+      first= word.slice(0)
+      word.slice!(0)
+      word += first
+      word += "ay"
+
+      else
+
+      word += "way"
+
+      end
+    end
+
+    newarray.join(" ")
+
+  # else
+  # end
 
   end
 end
